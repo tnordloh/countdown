b36a714b690450d4757d90ff03d530e1a22465be
Timothy Nordloh <tnordloh@gmail.com>
2015-07-20 15:04:47 -0600

    solved, mostly

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,67 @@
-class CoutDown
-  def initialize(goal, list*)
+class CountDown
+  POSSIBLE_ARGUMENTS = :divmod,:+,:-,:/,:*
+  def initialize(goal, *list,previous: "")
     @goal = goal
     @list = list
+    @found = false
+    @previous = previous
+    @next_level = []
+  end
 
+  attr_reader :previous
+
+  def try_all_combinations
+    @next_level = []
+    @list.combination(2).to_a.each do |args|
+      args = args.sort.reverse
+      try(args[0], :/, args[1]  ) unless ( args[0] == 0 &&
+                                           args[0] % args[1] != 0 )
+      try(args[0], :* , args[1] )
+      try(args[0], :+ , args[1] )
+      try(args[0], :- , args[1] )
+      return @found if @found == true
+    end
+    if @found == false
+      @next_level.each do |cd|
+        if cd.found?
+          @found = true
+          @answer = cd.previous + cd.answer
+          return true
+        end
+      end
+    end
+  end
+
+  def try(first,operator,second)
+    answer = first.send(operator,second)
+    answer_text = "#{first} #{operator.to_s} #{second} = #{answer}"
+    if answer == @goal
+      @answer = answer_text
+      @found = true
+    elsif @list.size > 2
+      next_node(first,second,answer,answer_text)
+    end
+  end
+
+  def next_node(first,second,answer,answer_text)
+    list = @list.dup
+    list.delete(first)
+    list.delete(second)
+    list << answer
+    @next_level << self.class.new(@goal, *list, previous: answer_text + ",")
+  end
+
+  def found?
+    try_all_combinations
+    @found
   end
 
   def answer
-    "#{list[0]} * #{list[1]} = @goal"
+    if found? == true
+      @answer
+    else
+      nil
+    end
   end
+
 end
--- test/count_down_test.rb
+++ test/count_down_test.rb
@@ -1,7 +1,19 @@
 require 'minitest/autorun'
 
-require_relative '../lib/countdown'
+require_relative '../lib/count_down'
 
-it "finds the closest answers" do
-  Countdown.new(10,5,2).answer.must_equal("5 * 2 = 10")
+describe CountDown do
+  it "finds the closest answers" do
+    CountDown.new(10,5,2).found?.must_equal(true)
+    CountDown.new(10,5,2).answer.must_equal("5 * 2 = 10")
+    CountDown.new(5,4,1).found?.must_equal(true)
+    CountDown.new(5,4,1).answer.must_equal("4 + 1 = 5")
+    CountDown.new(3,6,2).found?.must_equal(true)
+    CountDown.new(3,6,2).answer.must_equal("6 / 2 = 3")
+    CountDown.new(1,2,1).found?.must_equal(true)
+    CountDown.new(1,2,1).answer.must_equal("2 - 1 = 1")
+    CountDown.new(10,4,1,2).found?.must_equal(true)
+    CountDown.new(10,4,1,2).answer.must_equal("4 + 1 = 5,5 * 2 = 10")
+    CountDown.new(13,4,1,2,3).answer.must_equal("4 + 1 = 5,5 * 2 = 10")
+  end
 end
